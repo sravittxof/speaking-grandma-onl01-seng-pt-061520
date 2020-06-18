56a8c3262ddebc969f28c008b93097493ac74ffa
Travis Fox <travis.fox90@gmail.com>
2020-06-18 22:54:13 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -14,9 +14,9 @@
 def speak_to_grandma(hi_grandma)
   if hi_grandma == "I LOVE YOU GRANDMA!"
     "I LOVE YOU TOO PUMPKIN!"
-  elsif speak_to_grandma != speak_to_grandma.upcase
-    "HUH?! SPEAK UP, SONNY!"
-  else
+  elsif hi_grandma == hi_grandma.upcase
     "NO, NOT SINCE 1938!"
+  else
+    "HUH?! SPEAK UP, SONNY!"
   end
 end
